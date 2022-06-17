17f8375f5d68d266dc6a3eabb95cea316af5819c
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2022-06-17 18:51:01 -0500

    Actualizacion de varias funciones

--- server/functions/jest.config.js
+++ server/functions/jest.config.js
@@ -3,7 +3,11 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts","!<rootDir>/src/**/*.exceptions.ts"],
-  
-  testTimeout: 10000
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/**/*.exceptions.ts",
+    "!<rootDir>/src/**/index.ts",
+  ],
+
+  testTimeout: 10000,
 };
