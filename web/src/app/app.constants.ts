4687fcf410bbc6bcae05243952839d0f987ccb54
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2022-07-11 13:01:29 -0500

    Upload all files just at the end of the day. This is a general message.

--- web/src/app/app.constants.ts
+++ web/src/app/app.constants.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_PROFILE_IMG = 'assets/default-player-profile.png'
+export const DEFAULT_PROFILE_IMG = 'assets/default-player-profile.jpg'
 export const DEFAULT_SHIELD_IMG = 'assets/badge-team.png'
 
 export const BUCKET_NAME = 'deporty-app.appspot.com'
